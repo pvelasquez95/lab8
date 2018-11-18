5bc175f3bb5c8f91dc76da13d0b122837fac8061
pvelasquez95 <pablovelasquez95@hotmail.com>
2018-11-18 13:34:22 -0600

    redis finally working

--- lab8/MyExpressApp/routes/index.js
+++ lab8/MyExpressApp/routes/index.js
@@ -5,21 +5,21 @@ var objectId = require('mongodb').ObjectID;
 var app = express();
 var assert = require('assert');
 var url = 'mongodb://localhost:27017/juegos';
-//var redis = require("redis");
-//var redis_client = redis.createClient();
+var redis = require("redis");
+var cache = require('express-redis-cache')({
+  host: 'localhost', port: 6379
+});
 var db;
 
-/*var cache = require('express-redis-cache')({
-  port:6379,
-  host: 'localhost',
-  auth_pass:null,
-  db:0,
-  prefix:'home',
-  enabled:true
-});*/
+cache.on('error', function (error) {
+  console.log('Cache error!' + error);
+  return
+});
 
-//const redis_url = process.env.redis_url;
-//const client = redis.createClient(redis_url);
+
+
+const redis_url = process.env.redis_url;
+const client = redis.createClient(redis_url);
 
 app.listen(3001)
 console.log('Log listening on port 3001...')
@@ -32,16 +32,18 @@ app.use(function (req, res, next) {
 })
 
 app.use(cors());
-
 //app.engine('handlebars', exphbs({ defaultlayout: 'main'}));
 //app.set('cache',cache);
 //app.use(bodyparser.json());
 //app.use('/', juegos)
 
-app.get('/api/juegos/', function (req, res, next) {
+
+
+app.get('/api/juegos/', cache.route({ expire: 60, name: 'getAll' }), function (req, res, next) {
   mongo.connect(url, function (err, client) {
     if (!err) {
       console.log("We are connected");
+
       db = client.db('juegos');
     }
     db.collection('juegos').find().toArray((err, result) => {
@@ -66,38 +68,42 @@ app.get('/api/juegos/:id', function (req, res, next) {
 });
 
 app.post('/api/juegos/', function (req, res, next) {
+  console.log(JSON.stringify(req.body))
+  
+  const juego = {
+    name: req.body.name
+    , platform: req.body.platform
+    , rate: req.body.rate
+    , photo: req.body.photo
+  };
+
   mongo.connect(url, function (err, client) {
+    db = client.db('juegos');
     if (!err) {
       console.log("We are connected");
-      db = client.db('juegos');
-    }
-    const juego = {
-      name: req.body.name
-      , platform: req.body.platform
-      , memo: req.body.memo
-      , rate: req.body.rate
-      , photo: req.body.photo
+
     }
     db.collection('juegos').insertOne(juego, function (err, result) {
-      assert.equal(null, err);
-      if (err) return console.log(err);
-      //redis_insert(result.insertedId, result.ops[0]);
-      console.log("Item inserted");
-      res.status(201).send(result);
+      cache.del('getAll', (err, juego) => {
+        assert.equal(null, err);
+        if (err) return console.log(err);
+        console.log("Item inserted");
+        res.status(201).json(data)
+      });
       client.close();
     });
   });
 });
 
 app.put('/api/juegos/', function (req, res, next) {
-  var id = req.body.id;
+  var id = req.body._id;
   const item = {
     name: req.body.name
     , platform: req.body.platform
-    , memo: req.body.memo
     , rate: req.body.rate
     , photo: req.body.photo
   }
+  console.log("id " + id);
   mongo.connect(url, function (err, client) {
     if (!err) {
       console.log("We are connected");
@@ -114,14 +120,16 @@ app.put('/api/juegos/', function (req, res, next) {
 
 app.delete('/api/juegos/:id', function (req, res, next) {
   var id = req.params.id;
-  redis_delete(id);
   mongo.connect(url, function (err, client) {
     if (!err) {
       console.log("We are connected");
       db = client.db('juegos');
     }
+
+    console.log("id: " + req.params.id);
+    console.log("err: " + err);
     db.collection('juegos').deleteOne({ "_id": objectId(id) }, function (err, result) {
-      assert.equal(null, err);
+      console.log("result: " + result);
       if (err) return console.log(err)
       console.log('Item deleted');
       res.status(204).send(result);
@@ -129,13 +137,3 @@ app.delete('/api/juegos/:id', function (req, res, next) {
     });
   });
 });
-
-function redis_insert(id, object) {
-  console.log("Record from redis inserted");
-  redis_client.set(id, JSON.stringify(object), redis.print);
-}
-
-function redis_delete(id) {
-  console.log("Record from redis deleted");
-  redis_client.del(id);
-}
